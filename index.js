06e0aab67426003ce3f499d48daebe2ac6b34896
jonathan warner <j.m.warners@gmail.com>
2020-08-31 15:12:59 -0700

    GET GET and POST complete

--- index.js
+++ index.js
@@ -22,13 +22,43 @@ let users = [
     },
 ];
 
+// Configuring server
 server.get('/', (req,res) => {
-    res.status(200).json({ hello: "Node 33!!" });
+    res.status(200).json({ hello: "Hello World!" });
 });
 
+// GET Return an array of users
 server.get('/api/users', (req, res) => {
-    res.status(200).json({ data:hubs });
+    res.status(200).json({ data:users });
 });
+
+// GET Return a user with a specific id
+server.get('/api/users', (req, res) => {
+    res.status(200).json({ data:users.id })
+}); 
+
+// POST Create a new user
+server.post('/api/users', (req, res) => {
+    const newUser = req.body; 
+    newUser.id = shortid.generate()
+    users.push(newUser)
+    res.status(201).json({ message: newUser })
+    if(!newUser){
+        res.status(404).json({ errorMessage: "Please provide name and bio for the user." })
+    } else {
+        res.status(500).json({ errorMessage: "Uh-oh! Something's gone wrong adding the user!"})
+    }
+});
+
+// DELETE Remove user by id and return deleted user
+server.delete('/api/users/:id', (req, res) => {
+
+})
+
+// PUT Update the user by id
+server.put('/api/users/:id', (req, res) => {
+
+})
 
 const port = 8000;
 server.listen(port, () => console.log("Server is up..."));
