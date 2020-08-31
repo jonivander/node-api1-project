48072ed9eaad93b9537452282438f339ec0d4baa
jonathan warner <j.m.warners@gmail.com>
2020-08-31 15:47:53 -0700

    Post updated

--- index.js
+++ index.js
@@ -7,23 +7,28 @@ server.use(express.json());
 let users = [
     {
         id: 1,
-        name: "Item One",
-        bio: "node 33", 
+        name: "Jodie Whittacre",
+        bio: "13th Doctor", 
     },
     {
         id: 2,
-        name: "Item Two",
-        bio: "node 33", 
+        name: "Peter Capaldi",
+        bio: "12th Doctor", 
     },
     {
         id: 3,
-        name: "Item Three",
-        bio: "node 33", 
+        name: "Matt Smith",
+        bio: "11th Doctor", 
+    },
+    {
+        id: 4,
+        name: "David Tennant",
+        bio: "10th Doctor", 
     },
 ];
 
 // Configuring server
-server.get('/', (req,res) => {
+server.get('/', (req, res) => {
     res.status(200).json({ hello: "Hello World!" });
 });
 
@@ -33,18 +38,30 @@ server.get('/api/users', (req, res) => {
 });
 
 // GET Return a user with a specific id
-server.get('/api/users', (req, res) => {
-    res.status(200).json({ data:users.id })
+server.get('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id); 
+
+    const user = users.filter(user => user.id === id);
+    if(user){
+        res.status(200).json({ data:users.id })
+    } if(!user) {
+        res.status(404).json({ errorMessage: "User not found." })
+    } else { 
+        res.status(500).json({ errorMessage: "Uh-oh! Something's gone wrong! My fault!"})
+    }
+    
 }); 
 
 // POST Create a new user
 server.post('/api/users', (req, res) => {
     const newUser = req.body; 
     newUser.id = shortid.generate()
-    users.push(newUser)
-    res.status(201).json({ message: newUser })
-    if(!newUser){
-        res.status(404).json({ errorMessage: "Please provide name and bio for the user." })
+    
+    if(newUser.name === '' || newUser.bio === ''){
+        res.status(400).json({ errorMessage: "Bad request, BAD!" })
+    } if(newUser){
+        res.status(201).json({ message: newUser });
+        users.push(newUser)
     } else {
         res.status(500).json({ errorMessage: "Uh-oh! Something's gone wrong adding the user!"})
     }
@@ -52,7 +69,9 @@ server.post('/api/users', (req, res) => {
 
 // DELETE Remove user by id and return deleted user
 server.delete('/api/users/:id', (req, res) => {
+    const id = Number(req.params.id);
 
+    const users = users
 })
 
 // PUT Update the user by id
